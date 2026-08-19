bcfea6cb08975dfc6c227a0b07c315c81ae4a861
Вячеслав <despl1t@icloud.com>
2026-08-19 12:53:12 +0400

    WriterToFile

---
+++ src/file_reader_writer/Writer.java
@@ -0,0 +1,34 @@
+package file_reader_writer;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+//TODO реализовать запись отсортированных данных в файл с возможностью добавление данных в файл и его перезаписи
+
+    //Записывает коллекцию автомобилей в файл в режиме добавления
+    //Каждая строка типа: hoursePower, model, year
+
+public class Writer {
+
+    public static void appendCarsToFile(String path, CarList list)) throws IOExceprion {
+
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(path, true))) {
+            for (int i = 0; i < list.size(); i++) {
+                Car car = list.get(i);
+                writer.write(car.geHorsePower+ ", " + car.getModel() + ", " + car.getYear());
+                writer.newLine();
+            }
+        }
+    }
+    public static void overwriteCarsToFile(String path, CarList list) throws IOException {
+
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(path, false))) {
+            for (int i = 0; i < list.size(); i++) {
+                Car car = list.get(i);
+                writer.write(car.getHorsePower() + ", " + car.getModel() + ", " + car.getYear());
+                writer.newLine();
+            }
+        }
+    }
+}

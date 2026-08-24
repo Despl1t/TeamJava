22fb0f46fafe43954aff7b21f2bbd7b577dfd426
Вячеслав <despl1t@icloud.com>
2026-08-24 16:05:34 +0400

    +FixValidationReader+CarList

--- src/fileReaderWriter/Reader.java
+++ src/fileReaderWriter/Reader.java
@@ -7,6 +7,7 @@ import builder.Builder;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,54 +18,63 @@ public class Reader {
     // формат строки horsepower,model,year
     // Пример: 200,BMW X5,2020
 
-    public static CarList readCarsFromFile(String path) throws IOException{
-        CarList carList = new CarList(16);
+    public static CarList readCarsFromFile() throws IOException {
+        Path path = Path.of("CarList.txt");
+        CarList carList = new CarList();
         List<String> errors = new ArrayList<>();
 
-        try(BufferedReader buffer = new BufferedReader(new FileReader(path))){
+        try (BufferedReader buffer = new BufferedReader(new FileReader(path.toString()))) {
             String line;
             int lineNum = 0;
-                while((line = buffer.readLine()) != null) {
-                    lineNum++;
+            while ((line = buffer.readLine()) != null) {
+                lineNum++;
 
-                    if (line.trim().isEmpty() || line.startsWith("#")) {
-                        continue;
-                    }
-
-                    String[] parts = line.trim().split(",");
-                    if (parts.length != 3) {
-                        errors.add("Строка" + lineNum + ":ожидалось 3 поля, получено: " + parts.length);
-                        continue;
-                    }
-
-                    try {
-                        int Power = Integer.parseInt(parts[0].trim());
-                        String model = parts[1].trim();
-                        int year = Integer.parseInt(parts[2].trim());
-
-                        Car candidate = new Builder()
-                                .setPower(Power)
-                                .setModel(model)
-                                .setYear(year)
-                                .build();
-
-                        if (!CarValidation.isValid(candidate)) {
-                            var validationErrors = CarValidation.isValid(candidate);
-                            errors.add("Строка " + lineNum + "не прошла валидацию: " + String.join("; ", validationErrors));
-                            continue;
-                        }
-
-                        carList.add(candidate);
-
-                   }catch (NumberFormatException e) {
-                        errors.add("Строка " + lineNum + ": ошибка чтения '" + line +"'");
-                    }
+                if (line.trim().isEmpty() || line.startsWith("#")) {
+                    continue;
                 }
+
+                String[] parts = line.trim().split(",");
+                if (parts.length != 3) {
+                    errors.add("Строка" + lineNum + ":ожидалось 3 поля, получено: " + parts.length);
+                    continue;
+                }
+
+                try {
+                    int power = Integer.parseInt(parts[0].trim());
+                    String model = parts[1].trim();
+                    int year = Integer.parseInt(parts[2].trim());
+
+                    Car candidate = new Builder()
+                            .setPower(power)
+                            .setModel(model)
+                            .setYear(year)
+                            .build();
+
+                    if (!CarValidation.isValidPower(power)) {
+                        String errorMessage = "Некорректно указана мощность";
+                        errors.add("Строка " + lineNum + " не прошла валидацию: " + errorMessage);
+                        continue;
+
+                    } else if (!CarValidation.isValidModel(model)) {
+                        String errorMessage = "Некорректное название модели";
+                        errors.add("Строка " + lineNum + " не прошла валидацию: " + errorMessage);
+                        continue;
+                    } else if (!CarValidation.isValidYear(year)) {
+                        String errorMessage = "Некорректно указан год";
+                        errors.add("Строка " + lineNum + " не прошла валидацию: " + errorMessage);
+                        continue;
+                    }
+                    carList.add(candidate);
+
+                } catch (NumberFormatException e) {
+                    errors.add("Строка " + lineNum + ": ошибка чтения '" + line + "'");
+                }
+            }
         }
 
         if (!errors.isEmpty()) {
             StringBuilder sb = new StringBuilder();
-            sb.append("Файла прочитан с ошибками:\n");
+            sb.append("Файл прочитан с ошибками:\n");
             errors.forEach(e -> sb.append("- ").append(e).append("\n"));
             System.out.print(sb.toString());
         }

9e8d7ae12dbabf51fac179d14f304dd3bc9c572a
Вячеслав <despl1t@icloud.com>
2026-08-24 16:59:04 +0400

    +FixWriter

--- src/fileReaderWriter/Writer.java
+++ src/fileReaderWriter/Writer.java
@@ -9,29 +9,31 @@ import java.nio.file.Path;
 
 //TODO реализовать запись отсортированных данных в файл с возможностью добавление данных в файл и его перезаписи
 
-    //Записывает коллекцию автомобилей в файл в режиме добавления
-    //Каждая строка типа: Power, model, year
+//Записывает коллекцию автомобилей в файл в режиме добавления
+//Каждая строка типа: Power, model, year
 
 public class Writer {
 
-    public static void appendCarsToFile(Path path, CarList list) throws IOExceprion {
+    public static void appendCarsToFile(CarList list) throws IOException {
+        Path path = Path.of("SortedCarsList.txt");
 
         try(BufferedWriter writer = new BufferedWriter(new FileWriter(path.toString(), true))) {
             for (int i = 0; i < list.size(); i++) {
                 Car car = list.get(i);
-                writer.write(car.getPower() + ", " + car.getModel() + ", " + car.getYear());
+                writer.write(car.getPower() + "," + car.getModel() + "," + car.getYear());
                 writer.newLine();
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
-    public static void overwriteCarsToFile(String path, CarList list) throws IOException {
+    public static void overwriteCarsToFile(CarList list) throws IOException {
+        Path path = Path.of("SortedCarsList.txt");
 
-        try(BufferedWriter writer = new BufferedWriter(new FileWriter(path, false))) {
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(path.toString(), false))) {
             for (int i = 0; i < list.size(); i++) {
                 Car car = list.get(i);
-                writer.write(car.getPower() + ", " + car.getModel() + ", " + car.getYear());
+                writer.write(car.getPower() + "," + car.getModel() + "," + car.getYear());
                 writer.newLine();
             }
         }
